a5edd7986a51348d4ce960c14390df879553a751
bSharpd <30420213+bSharpd@users.noreply.github.com>
2021-09-07 20:49:50 -0400

    stashing stuff

--- scripts/midi.js
+++ scripts/midi.js
@@ -1,26 +1,3 @@
-/**
- * message.data = Uint8Array containing the data bytes of a single MIDI message
- * midi specs: midi.org/specifications-old/item/table-1-summary-of-midi-message
- * 
- * values in decimal by default, convert to hex when referencing specs
- * 
- * message.data[0]: status/event (e.g. 144:note on, 128:note off)
- * 
- * data[1,2] content/formats depends on the event
- * some events won't return data[2] at all (e.g. 208:after-touch, only returns pressure value)
- * 
- * when data[0] = 144 || 128:
- * message.data[1]: note number
- * message.data[2]: velocity 0-127
- * 
- * other data[0] values:
- * 248: Timing Clock. Sent 24 times per quarter note
- * 254: Active Sensing. Sent repeatedly to tell the receiver that connection is alive
- * 
- * For my own keyboard, middle C is note 60- this demo utilizes 2 octaves
- * 
- */
-
 function midiNoteToFrequency (note) {
     return Math.pow(2, ((note - 69) / 12)) * 440;
 }
@@ -47,6 +24,21 @@ function pressNote(midiArray){
     }
 }
 
+let wav = ['sine', 'square', 'sawtooth', 'triangle'];
+
+//pitch bend multipliers (5th up/down, octave up/down, 3rd up/down)
+let pb = [1, 1.5, 0.75, 2, 0.5, 1.26, 0.79];
+
+const abcDict = ['2','4','8','[',']','(3','z','|',' ',
+'C,,','_D,,','D,,','_E,,','E,,','F,,','^F,,','G,,','_A,,','A,,','_B,,','B,,',
+'C,','_D,','D,','_E,','E,','F,','^F,','G,','_A,','A,','_B,','B,',
+'C','_D','D','_E','E','F','^F','G','_A','A','_B','B',
+'c','_d','d','_e','e','f','^f','g','_a','a','_b','b',
+'c,','_d,','d,','_e,','e,','f,','^f,','g,','_a,','a,','_b,','b,'
+];
+
+let handStaff = 'right';
+
 function playNote (frequency,type) {
     oscillators[frequency] = context.createOscillator();
     oscillators[frequency].type = type;
@@ -59,43 +51,24 @@ function stopNote (frequency) {
     oscillators[frequency].stop(context.currentTime);
     oscillators[frequency].disconnect();
 }
-let wav = ['sine', 'square', 'sawtooth', 'triangle'];
-//pitch bend multipliers (5th up/down, octave up/down, 3rd up/down)
-let pb = [1, 1.5, 0.75, 2, 0.5, 1.26, 0.79];
-
-//save notes + timestamps
-let midi_history = [];
 
 let editMode = false;
 function abcToggle(){
     editMode = !editMode;
 }
-const abcDict = ['2','4','8','[',']','(3','z','|',' ',
-'C,,','_D,,','D,,','_E,,','E,,','F,,','^F,,','G,,','_A,,','A,,','_B,,','B,,',
-'C,','_D,','D,','_E,','E,','F,','^F,','G,','_A,','A,','_B,','B,',
-'C','_D','D','_E','E','F','^F','G','_A','A','_B','B',
-'c','_d','d','_e','e','f','^f','g','_a','a','_b','b',
-'c,','_d,','d,','_e,','e,','f,','^f,','g,','_a,','a,','_b,','b,'
-];
 
-let handStaff = 'right';
 
 function editAbc(midiNote){
     switch(midiNote[1]){
         case 21:
             handStaff = 'left';
             break;
-            case 23:
+        case 23:
             handStaff = 'right';
-            break;
-            case 96:
-            abcToggle();
-            break;
-            case 97:
-            abcRender();
             break;
         default:
             document.getElementById(handStaff).value += abcDict[midiNote[1]-27];
+            abcRender();
     }
 }
 
@@ -133,11 +106,6 @@ function makeSynth(){
         };
     });
 }
-
-function inputsToAbc(){
-
-}
-
 /*
 ** ABC notation reference guide
 ** = natural
